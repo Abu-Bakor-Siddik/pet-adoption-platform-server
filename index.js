4a20c0620fb283a123e07e6745b93de1607f88df
Abu Bakor Siddik (Shuvo) <abu.bakor.cse1100@gmail.com>
2026-08-18 18:12:57 +0600

    Connected to mongodb with 3 collection

--- index.js
+++ index.js
@@ -1,40 +1,58 @@
-const express = require ('express')
-const dotenv = require('dotenv')
-const { MongoClient, ServerApiVersion } = require('mongodb');
-dotenv.config()
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+dotenv.config();
 
 const uri = process.env.MONGODB_URI;
 
-const app = express()
+const app = express();
 const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server
     await client.connect();
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+
+    // Connect to the database
+    const database = client.db("pet_adoption");
+
+    // Connect to the collections
+    const usersCollection = database.collection("users");
+    const petsCollection = database.collection("pets");
+    const adoptionRequestsCollection = database.collection("adoptionRequests");
+
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
   }
 }
+
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Server is running fine!");
+});
 
-app.get('/',(req,res)=>{
-    res.send("Server is running fine!")
-})
-
-app.listen(PORT,() =>{
-    console.log(`Server is running on port : ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port : ${PORT}`);
+});
